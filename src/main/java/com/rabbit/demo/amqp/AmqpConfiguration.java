ce4f62795baa89eef620a432a77a9699bec9d0da
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-02-05 19:57:43 -0300

    init

---
+++ src/main/java/com/rabbit/demo/amqp/AmqpConfiguration.java
@@ -0,0 +1,31 @@
+package com.rabbit.demo.amqp;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AmqpConfiguration {
+
+    @Bean
+    public Queue createQueue() {
+        return new Queue("test.publisher", false);
+//      return QueueBuilder.nonDurable("test.publisher").build();
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin) {
+        return  event -> rabbitAdmin.initialize();
+    }
+}
